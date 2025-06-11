3dc09dd3c082407e9111773e5ab7810e298a96d2
Evan Gamble <762417+egamble@users.noreply.github.com>
2025-06-11 13:04:06 +0200

    allow empty lines in ties file

--- src/in_files.rs
+++ src/in_files.rs
@@ -21,43 +21,47 @@ pub fn build_ties_map(ties_path: &String) ->
   let reader = create_reader(ties_path);
 
   for line_result in reader.lines() {
-    if let Ok(line) = line_result {
-      let split: Vec<&str> = line.split("\t").collect();
+    if let Ok(mut line) = line_result {
+      line = line.trim().to_string();
+      
+      if line != "" {
+        let split: Vec<&str> = line.split("\t").collect();
 
-      match split[..] {
-        [
-          from_x_str,
-          from_y_str,
-          from_z_str,
-          from_realm_str,
-          from_direction_str,
-          to_x_str,
-          to_y_str,
-          to_z_str,
-          to_realm_str,
-          to_direction_str
-        ] => {
-          let from_block_coords = (
-            coord_from_str(from_x_str),
-            coord_from_str(from_y_str),
-            coord_from_str(from_z_str),
-            realm_from_str(from_realm_str)
-          );
+        match split[..] {
+          [
+            from_x_str,
+            from_y_str,
+            from_z_str,
+            from_realm_str,
+            from_direction_str,
+            to_x_str,
+            to_y_str,
+            to_z_str,
+            to_realm_str,
+            to_direction_str
+          ] => {
+            let from_block_coords = (
+              coord_from_str(from_x_str),
+              coord_from_str(from_y_str),
+              coord_from_str(from_z_str),
+              realm_from_str(from_realm_str)
+            );
 
-          let to_block_coords = (
-            coord_from_str(to_x_str),
-            coord_from_str(to_y_str),
-            coord_from_str(to_z_str),
-            realm_from_str(to_realm_str)
-          );
+            let to_block_coords = (
+              coord_from_str(to_x_str),
+              coord_from_str(to_y_str),
+              coord_from_str(to_z_str),
+              realm_from_str(to_realm_str)
+            );
 
-          let from_direction = Direction::direction_from_str(from_direction_str);
-          let to_direction = Direction::direction_from_str(to_direction_str);
+            let from_direction = Direction::direction_from_str(from_direction_str);
+            let to_direction = Direction::direction_from_str(to_direction_str);
           
-          ties_map.insert(from_block_coords, (to_block_coords, from_direction, to_direction));
-        },
-        _ => {
-          exit!("Error reading from ties file {:?}, line: {}", ties_path, line);
+            ties_map.insert(from_block_coords, (to_block_coords, from_direction, to_direction));
+          },
+          _ => {
+            exit!("Error reading from ties file {:?}, line: {}", ties_path, line);
+          }
         }
       }
     } else {
